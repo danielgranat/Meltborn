6a6c5b87d286a3b78ad4b313a91a29b7982fde17
daniel <daniel.g@diglobal.tech>
2023-06-21 21:32:48 +0300

    Unremark FX

--- Assets/Scripts/Elements/FXManager.cs
+++ Assets/Scripts/Elements/FXManager.cs
@@ -16,7 +16,8 @@ public class FXManager : MonoBehaviour
     
     public void showPop(Vector3 pos)
     {
-        //pop.gameObject.GetComponent<Pop>().show(pos); //con esta linea se llama al metodo show de la clase Pop
+        if(pop)
+            pop.gameObject.GetComponent<Pop>().show(pos); //con esta linea se llama al metodo show de la clase Pop
     }
 
     private void OnDestroy()
